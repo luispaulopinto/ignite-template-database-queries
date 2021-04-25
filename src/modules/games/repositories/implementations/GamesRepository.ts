4ab86c273c2ec228362a3f09140f053fbceafc2f
Luis Paulo Pinto <lp_paulo85@yahoo.com.br>
2021-04-25 14:06:31 -0300

    Initial Commit

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -1,9 +1,9 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Like, Repository } from "typeorm";
 
-import { User } from '../../../users/entities/User';
-import { Game } from '../../entities/Game';
+import { User } from "../../../users/entities/User";
+import { Game } from "../../entities/Game";
 
-import { IGamesRepository } from '../IGamesRepository';
+import { IGamesRepository } from "../IGamesRepository";
 
 export class GamesRepository implements IGamesRepository {
   private repository: Repository<Game>;
@@ -15,16 +15,43 @@ export class GamesRepository implements IGamesRepository {
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
       .createQueryBuilder()
-      // Complete usando query builder
+      .where("title ILIKE :title", { title: `%${param}%` })
+      .getMany();
+
+    // *********** OR ***********
+
+    // return this.repository.query(
+    //   `
+    //     SELECT
+    //      "id",
+    //      "title",
+    //      "created_at",
+    //      "updated_at"
+    //     FROM
+    //      "games" "Game"
+    //     WHERE
+    //       title ILIKE '%' || $1 || '%'
+    // `,
+    //   [param]
+    // );
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      `
+      SELECT
+        COUNT(title) AS "count"
+      FROM
+        "games" "Game"
+    `
+    );
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder("games")
+      .relation(Game, "users")
+      .of(id)
+      .loadMany();
   }
 }
--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,8 +1,8 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from "typeorm";
 
-import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
-import { User } from '../../entities/User';
-import { IUsersRepository } from '../IUsersRepository';
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from "../../dtos";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
 
 export class UsersRepository implements IUsersRepository {
   private repository: Repository<User>;
@@ -14,17 +14,75 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne({
+      relations: ["games"],
+      where: [{ id: user_id }],
+    });
+
+    if (!user) return new User();
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      `
+        SELECT
+         "id", 
+         "first_name", 
+         "last_name", 
+         "email", 
+         "created_at", 
+         "updated_at"
+        FROM
+          "users" "User"
+        ORDER BY
+          "first_name" ASC
+    `
+    );
+
+    // *********** OR ***********
+
+    // return this.repository.find({
+    //   order: {
+    //     first_name: "ASC",
+    //   },
+    // });
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      `
+        SELECT
+         "id",
+         "first_name",
+         "last_name",
+         "email",
+         "created_at",
+         "updated_at"
+        FROM
+          "users" "User"
+        WHERE
+          LOWER(first_name) LIKE $1
+            AND
+          LOWER(last_name) LIKE $2
+    `,
+      [first_name.toLowerCase(), last_name.toLowerCase()]
+    );
+
+    // *********** OR ***********
+
+    // return this.repository
+    //   .createQueryBuilder()
+    //   .where("LOWER(first_name) LIKE :first_name", {
+    //     first_name: first_name.toLocaleLowerCase(),
+    //   })
+    //   .where("LOWER(last_name) LIKE :last_name", {
+    //     last_name: last_name.toLocaleLowerCase(),
+    //   })
+    //   .getMany();
   }
 }
